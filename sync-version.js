f9fc814a661e7cea8ab2572a3dc2b652a19b38ea
Adar Fhlisher <adarfhlisher@Adars-MacBook-Pro.local>
2026-03-24 19:01:00 +0200

    chore: add ghversion to info.json for auto updates

--- sync-version.js
+++ sync-version.js
@@ -1,11 +1,19 @@
+// sync-version.js
 const fs = require('fs');
 const path = require('path');
 
 const pkg = require('./package.json');
-const infoPath = path.join(__dirname, 'Video-Upscaler.iinaplugin', 'Info.json');
+const infoPath = path.join(__dirname, 'GPU-Upscaler.iinaplugin', 'Info.json');
 
 const info = JSON.parse(fs.readFileSync(infoPath, 'utf8'));
+
 info.version = pkg.version;
 
+if (typeof info.ghVersion === 'number') {
+  info.ghVersion += 1;
+} else {
+  info.ghVersion = 1;
+}
+
 fs.writeFileSync(infoPath, JSON.stringify(info, null, 2) + '\n');
-console.log(`Updated Info.json to version ${pkg.version}`);
+console.log(`Synced Info.json: version ${info.version}, ghVersion ${info.ghVersion}`);
